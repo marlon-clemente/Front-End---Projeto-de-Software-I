ee933090a1457c8d24dd1a70aad3fa46fa52b72e
emanoeldemoura <37151337+emanoeldemoura@users.noreply.github.com>
2020-05-03 00:34:00 -0300

    validação do formulário de login

--- src/pages/Login/AuthDir/index.js
+++ src/pages/Login/AuthDir/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useCallback } from 'react'
+import React, { useContext, useCallback, useState} from 'react'
 import {withRouter, Redirect} from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -11,20 +11,43 @@ import Styles from './styles';
 import loginApp from '../../../firebase';
 import { AuthContext } from '../../../context/Auth';
 
+// validação (email e senha ) e (validação TextField)
 const Login = ({history}) =>{
+        
     const classes = Styles();
-
+  
+    const [userInput, setUserInput] = useState('')
+    const [userInputEmail, setUserInputEmail] = useState('')
+    var [requiredEmail,setRequiredEmail]=useState(false)
+    var [requiredPassword,setRequiredPassword]=useState(false)
+ 
     const handleLogin = useCallback(
         async event => {
           event.preventDefault();
           const { email, password } = event.target.elements;
+          
+          if(email.value.length==0){           
+            setUserInputEmail("obrigatório")            
+            setRequiredEmail(true)
+          }else if(password.value.length==0){
+            setUserInput("obrigatório")
+            setRequiredPassword(true)                
+          }
+          
           try {
             await loginApp
               .auth()
               .signInWithEmailAndPassword(email.value, password.value);
             history.push("/");
           } catch (error) {
-            alert(error);
+            var errorCode = error.code;
+            if(errorCode=="auth/user-not-found"){
+               alert("usuario incorreto");                   
+            }else if(errorCode=="auth/wrong-password"){
+               alert("senha incorreta");                   
+            }else if(errorCode=="auth/invalid-email"){
+               alert(" e-mail invalido");           
+            }                                
           }
         },
         [history]
@@ -35,41 +58,49 @@ const Login = ({history}) =>{
     if(currentUser){
         return <Redirect to="/" />
     }
-
+  
     return(
-        <div className={classes.paper}>
+            
+       <div className={classes.paper}>
             <Avatar className={classes.avatar}>
                 <LockOutlinedIcon />
-            </Avatar><Typography component="h1" variant="h5">
-                 Faça login para continuar
-             </Typography>
+            </Avatar>
+            <Typography className={classes.titulo} component="h1" variant="h5" color="primary">
+                <b>Bem Vindo</b>
+            </Typography>
+            <Typography className={classes.subtitulo} component="h1" variant="body2" color="textSecondary">
+                <i>Faça login para continuar</i>
+            </Typography>                
+                 
              <form className={classes.form} 
-                onSubmit={handleLogin}
-                noValidate>
+                onSubmit={handleLogin}              
+                noValidate>                   
                     <TextField
                         variant="outlined" margin="normal"
                         required fullWidth
                         id="email" label="Endereço de e-mail"
-                        name="email" autoComplete="email"
-                        autoFocus
+                        name="email" autoComplete="email"                        
+                        autoFocus      
+                        helperText={userInputEmail}  
+                        error = {requiredEmail}                  
                     />
-                    <TextField
+                    <TextField                       
                         variant="outlined" margin="normal"
                         required fullWidth
                         name="password" label="Senha"
                         type="password" id="password"
-                        autoComplete="current-password"
-                    
+                        autoComplete="current-password"                                                                         
+                        helperText={userInput}   
+                        error = {requiredPassword}                                           
                     />
-
+                   
                  <Button
-                 fullWidth
-                 variant="contained"
-                 color="primary"
-                 className={classes.submit}
-                 type="submit"
-                 >
-                     Entrar
+                    fullWidth
+                    variant="contained"
+                    color="primary"                
+                    className={classes.submit}                                 
+                    type="submit"                
+                 > Entrar
                  </Button>
 
                  <Box mt={5}>
@@ -78,7 +109,6 @@ const Login = ({history}) =>{
                      </Typography>
                  </Box>
              </form>
-
         </div>
     )
 };
