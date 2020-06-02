975bcbc014f725fb5944c34b9b3cced451220e13
emanoeldemoura <emanoeldemoura87@gmail.com>
2020-06-01 22:52:16 -0300

    teste1

--- src/pages/Login/AuthDir/index.js
+++ src/pages/Login/AuthDir/index.js
@@ -104,7 +104,7 @@ const Login = ({history}) =>{
     <div className={classes.section_desktop}>
       <Typography component="div"><Box fontFamily="Lato"
         fontSize={25} fontWeight={400}
-        textAlign="center">BEM VINDO DIREÇÃO
+        textAlign="center"><b>BEM VINDO DIREÇÃO</b>
         </Box><Box fontFamily="Roboto"
         fontSize={18} fontWeight={300}
         textAlign="center">Efetue login para acessar
